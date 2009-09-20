e1dd8fb03b905a18837594f0aa8efd378513c2d4
pydave <pydave@gmail.com>
2009-09-19 18:49:16 -0700

    reasonably working version.
    only tested on 1up

---
+++ magic.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import os.path as path
+
+import magic_config as cfg
+
+def require_mp3cut():
+    """
+    Aborts if mp3cut isn't installed
+    """
+    # pipe to null so user can't see output
+    installed = os.system("which mp3cut > /dev/null") == 0
+    if not installed:
+        print "Error: mp3cut is not installed."
+        print "\t We use mp3cut to trim files and this program is useless without it."
+        print "\t Install with:\n\t sudo apt-get install mp3cut"
+        sys.exit(-1)
+
+
+def mp3cut(infile, min, sec, outfile):
+    """
+    mp3cut_command(str, int, int, str) --> None
+    Run mp3cut on the specified files
+
+    outfile - the file to output to
+    infile  - the source file
+    min,sec - where to start the song
+    """
+    # skip noops
+    if min + sec < 0:
+        return
+
+    command = "mp3cut -o '%s' -t %d:%d '%s'" % (outfile, min, sec, infile)
+    print command
+    os.system(command)
+
+
+def seconds_to_min_sec(seconds):
+    """
+    seconds_to_min_sec(int) --> int,int
+    Converts seconds to minutes and seconds
+
+    >>> seconds_to_min_sec(5)
+    (0, 5)
+    >>> seconds_to_min_sec(60)
+    (1, 0)
+    >>> seconds_to_min_sec(125)
+    (2, 5)
+    """
+    sec = seconds%60
+    min = seconds / 60
+    return min,sec
+    
+
+def get_start_time(podPath):
+    """
+    get_start_time(path) --> int,int
+    Returns the minutes and seconds for the current podcast path
+    """
+    try:
+        min, sec = seconds_to_min_sec(cfg.startTime[podPath])
+    except KeyError:
+        print "Warning: No start time set for", podPath
+
+    return min,sec
+
+
+def cut_files(fileNames, podPath, min, sec):
+    for f in fileNames:
+        sourceFilePath = path.join(podPath, f)
+        targetFilePath = path.join(os.getcwd(), f)
+        mp3cut(path.abspath(sourceFilePath), min, sec, path.abspath(targetFilePath))
+
+
+def main():
+    # abort if we have to
+    require_mp3cut()
+
+    # create a path walker
+    walker = os.walk("Podcasts")
+    # discard podcast-free parent directory
+    walker.next()
+
+    # cut stuff up
+    for podPath, dn, fileNames in walker:
+        print podPath
+        min,sec = get_start_time(podPath)
+        cut_files(fileNames, podPath, min, sec)
+        print
+
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    #_test()
+    main()
+
+
---
+++ magic_config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+startTime = {}
+startTime["Podcasts/1UP.com  Listen UP"] = 22
+startTime["Podcasts/A Life Well Wasted"] = 0
+startTime["Podcasts/Giant Bombcast"] = 0
+startTime["Podcasts/Mobcast"] = 0
+startTime["Podcasts/One Life Left » Podcast"] = 0
+startTime["Podcasts/Rebel FM"] = 0
+startTime["Podcasts/Search Engine"] = 0
+startTime["Podcasts/The Geekbox"] = 0
