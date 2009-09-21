d9e6991801db2d091bb17bacc1f2483ee0ec7826
pydave <pydave@gmail.com>
2009-09-20 20:44:10 -0700

    Update trim start times
    still missing rebel since there are regular episodes and game club

--- config.py
+++ config.py
@@ -7,12 +7,13 @@ targetFolder = '/home/dbriscoe/data/podcasts/Processed'
 # trim times
 startTime = {}
 startTime["ALifeWellWasted"] = 0
-startTime["Geekbox"] = 0
-startTime["GiantBombCast"] = 0
+startTime["Geekbox"] = 10
+startTime["GiantBombCast"] = 22
 startTime["ListenUp"] = 22
 startTime["MobCast"] = 0
+# TODO: figure out rebel since there is game club
 startTime["RebelFM"] = 0
-startTime["SearchEngine"] = 0
+startTime["SearchEngine"] = 13
 
 
 # manage.py
