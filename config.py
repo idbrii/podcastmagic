ea8ff7abb91b43bf0f6b679c1286eb3ddd03c213
pydave <pydave@gmail.com>
2010-03-26 22:36:49 -0700

    Move config settings around and add README
    Pushed all config settings that someone is likely to need to change at
    the top.
    
    Add a readme explaining the project.

--- config.py
+++ config.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# trim times (in seconds)
+startTime = {}
+startTime["ALifeWellWasted"] = 0
+startTime["Geekbox"] = 10
+startTime["GiantBombCast"] = 22
+startTime["ListenUp"] = 35
+startTime["MobCast"] = 50
+# both rebel and game club have long intros
+startTime["RebelFM"] = 29
+# TODO: figure out SearchEngine. it's outputting bad files. I used to cut at 13
+startTime["SearchEngine"] = 0
+
+# manage.py
+maxFilesToCopy = 10
+
 # Where your iPod is mounted
 iPod = '/media/YITHIAN/'
+
+
+
+
 
 # Where your rebuild_db script is located
 rebuild_db = iPod +'/rebuild_db.py'
@@ -15,19 +34,3 @@ listeningFolder = '/home/dbriscoe/data/podcasts/Listening'
 folders = [ trashFolder, newCastFolder, trimCastFolder, listeningFolder ]
 iPodCastFolder = iPod +'podcasts'
 freeSpaceMagic = iPod +'magic'
-
-# trim times
-startTime = {}
-startTime["ALifeWellWasted"] = 0
-startTime["Geekbox"] = 10
-startTime["GiantBombCast"] = 22
-startTime["ListenUp"] = 35
-startTime["MobCast"] = 50
-# both rebel and game club have long intros
-startTime["RebelFM"] = 29
-# TODO: figure out SearchEngine. it's outputting bad files. I used to cut at 13
-startTime["SearchEngine"] = 0
-
-
-# manage.py
-maxFilesToCopy = 10
